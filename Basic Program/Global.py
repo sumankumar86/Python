769ea80ebf531b9d3fa635bf3525690915cb598f
sumankumar86 <sumankumarghosh70@gmail.com>
2022-10-08 21:27:37 +0530

    new update

---
+++ Basic Program/Global.py
@@ -0,0 +1,19 @@
+def sum():
+    global x 
+    x="Anik"
+    print("My name is "+x)
+
+
+sum();
+print("My name is "+x)
+
+import random
+x=random.randrange(1,100)
+if(x%2==0):
+    print("Even")
+else:
+    print("Odd")
+
+x=5
+y=float(x)
+print(y)
---
+++ Basic Program/datatype.py
@@ -0,0 +1,30 @@
+# Differnet types of datatype
+x=5
+y="Suman"
+z=5.6522222
+m='C'
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(m))
+
+# multiple values in multiple variable
+a,b,c="Apple","Mango","Guava"
+print(a)
+print(b)
+print(c)
+
+# one value to multiple variables
+a=b=c="Mango"
+print(a)
+print(b)
+print(c)
+
+a,b,c="Apple","Mango","Guava"
+print(a,b,c)#print all values
+print(a+b+c)#Concatination
+
+x=5
+y="Python"
+# print(x+y) this typeof concatination is not allowed in python but it is allowed in java
+print(x,y)
